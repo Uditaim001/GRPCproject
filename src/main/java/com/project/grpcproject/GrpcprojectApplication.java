412a0379df76839219a911b53057fcbc8098f262
Udit <udit.shukla@gonuclei.com>
2024-11-25 21:46:43 +0530

    First commit

---
+++ src/main/java/com/project/grpcproject/GrpcprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.grpcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/grpcproject/GrpcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.grpcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
