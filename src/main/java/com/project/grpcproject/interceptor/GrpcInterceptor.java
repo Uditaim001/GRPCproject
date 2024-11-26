633ba9c650e799637dfb18b52bee08c617fd4e00
Udit <udit.shukla@gonuclei.com>
2024-11-26 19:16:55 +0530

    Implemented the GrpcInterceptor

--- src/main/java/com/project/grpcproject/interceptor/GrpcInterceptor.java
+++ src/main/java/com/project/grpcproject/interceptor/GrpcInterceptor.java
@@ -1,4 +1,35 @@
 package com.project.grpcproject.interceptor;
 
-public class GrpcInterceptor {
+import io.grpc.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+
+
+public class GrpcInterceptor implements ServerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(GrpcInterceptor.class);
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
+        logger.info("Call to method:{}", serverCall.getMethodDescriptor().getFullMethodName());
+        logger.info("Metadata: {}",metadata);
+        return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(serverCallHandler.startCall(serverCall, metadata)) {
+            @Override
+            public void onMessage(ReqT message) {
+                logger.info("Request:{}",message);
+                super.onMessage(message);
+            }
+            @Override
+            public void onComplete() {
+                logger.info("Call completed");
+                super.onComplete();
+            }
+
+            @Override
+            public void onHalfClose() {
+                logger.info("Requests Finished");
+                super.onHalfClose();
+            }
+        };
+    }
 }
--- src/main/java/com/project/grpcproject/server/GrpcServer.java
+++ src/main/java/com/project/grpcproject/server/GrpcServer.java
@@ -1,5 +1,6 @@
 package com.project.grpcproject.server;
 
+import com.project.grpcproject.interceptor.GrpcInterceptor;
 import com.project.grpcproject.service.UserService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -8,6 +9,7 @@ public class GrpcServer {
     public static void main(String[] args) throws Exception {
         Server server = ServerBuilder.forPort(50051)
                 .addService(new UserService())
+                .intercept(new GrpcInterceptor())
                 .build();
 
         System.out.println("Server started on port 50051");
